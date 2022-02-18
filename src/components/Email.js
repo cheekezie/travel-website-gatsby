36b8aeb153af868eb075fee11bcf1e462af884ba
cheekezie <harrychikezie1@gmail.com>
2022-02-18 07:22:46 +0100

    fixed email overflow

--- src/components/Email.js
+++ src/components/Email.js
@@ -59,7 +59,6 @@ export const EmailContent = styled.div`
 export const FormWrap = styled.div`
     display: flex;
     justify-content: center;
-    width: 100%;
     input{
         width: 350px;
         padding: 0 1.5rem;
